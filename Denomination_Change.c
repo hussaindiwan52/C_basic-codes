506b80dae6052c4ff7e847fe58ab24d9effc6008
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-09 20:10:59 +0530

    Update Denomination_Change.c

--- Denomination_Change.c
+++ Denomination_Change.c
@@ -4,16 +4,17 @@ int main()
     int money;
     printf("enter amount of money :- ");
     scanf("%d", &money);
-    int denomination[] = {100, 50, 10, 5, 1};
-    int bills[] = {0, 0, 0, 0, 0};
-    for (int i = 0; i < 5; i++)
+    int denomination[] = {100, 50, 10, 1};
+    int bills[] = {0, 0, 0, 0};
+    for (int i = 0; i < 4; i++)
     {
         bills[i] = money / denomination[i];
         money = money % denomination[i];
     }
     printf("\nThe change is:- \n");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("%d bills of rupees %d \n", bills[i], denomination[i]);
     }
+    printf("remaining amount is:-%d",bills[3], denomination[3]);
 }
