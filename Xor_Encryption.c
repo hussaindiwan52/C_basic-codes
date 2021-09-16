3dfc3bfb46f51197d5ce3bcd2bc4c7ab09f36eb9
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-16 14:01:52 +0530

    Update Xor_Encryption.c

--- Xor_Encryption.c
+++ Xor_Encryption.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-#define decrypt 5
+#define decrypt 555
 int main()
 {
-    int num,n,encrypted_num;
-    printf("enter number to be encrypted :-");
-    scanf("%d",&num);
-    encrypted_num=num^decrypt;
-    printf("number after encryption is = %d\n",encrypted_num);
+    int n;
+    char str[50];
+    printf("enter string to be encrypted :-");
+    scanf("%s",&str);
+    for(int i=0;i<50;i++){
+        str[i]=str[i]^decrypt;
+    }
+    printf("string after encryption is = %s\n",str);
     printf("\nenter decryption key :-");
     scanf("%d",&n);
-    encrypted_num=encrypted_num^(n);
-    printf("number after decryption is = %d\n",encrypted_num);
+    for(int i=0;i<50;i++){
+        str[i]=str[i]^decrypt;
+    }    
+    printf("string after decryption is = %s\n",str);
 
 }
