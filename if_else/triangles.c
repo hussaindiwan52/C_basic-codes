342ac40138c895e34e5093a7188d0c5ade26873a
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-20 18:35:34 +0530

    Update triangles.c

--- if_else/triangles.c
+++ if_else/triangles.c
@@ -29,5 +29,22 @@ int main(){
     else{
         printf("it is not a valid triangle\n");
     }
-    
+    printf("\n");
+    // 
+    int x1,x2,x3,y1,y2,y3,det;
+    printf("enter 1st point :-");
+    scanf("%d %d",&x1,&y1);
+    printf("enter 2nd point :-");
+    scanf("%d %d",&x2,&y2);
+    printf("enter 3rd point :-");
+    scanf("%d %d",&x3,&y3);
+    det=x1*(y2-y3)-y1*(x2-x3)+(x2*y3-y2*x3);
+    if(det==0){
+        printf("points are not the vertices of a triangle");
+    }
+    else{
+        printf("points are the vertices of a triangle");
+
+    }
+
 }
