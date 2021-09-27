b10b7204ff892adda0e67cc096d72037a7762800
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-28 01:53:18 +0530

    Update smallest_number_from_sum.c

--- loops&arrays/smallest_number_from_sum.c
+++ loops&arrays/smallest_number_from_sum.c
@@ -6,26 +6,42 @@ int main()
     printf("enter the number of digits and their sum :- ");
     scanf("%d %d",&digits,&sum);
     if(sum<=digits*9){
+        printf("smallest number whose digits sum up to %d =",sum);
         int nines=0;
         nines=sum/9;
-        if(nines<digits){
-            printf("%d",sum%9);
-            sum-=sum%9;
-            digits--;
+        if(nines!=0){
+            if(nines<digits){
+                printf("%d",sum%9);
+                sum-=sum%9;
+                digits--;
+            }
+            nines=sum/9;
+            sum=sum%9;
+            while(digits-nines!=0){
+                printf("%d",sum);
+                sum=sum/9;
+                digits--;
+            }
+            for (int i = 0; i < nines; i++) {
+                printf("9");
+            }
         }
-        nines=sum/9;
-        sum=sum%9;
-        while(digits-nines!=0){
+        else if(digits==1){
             printf("%d",sum);
-            sum=sum/9;
+        }
+        else{
+            printf("1");
             digits--;
+            while(digits-2>-1){
+                printf("0");
+                digits--;
+            }
+            printf("%d",sum-1);
         }
-        for (int i = 0; i < nines; i++) {
-            printf("9");
-        }
+        
     }
     else{
-        printf("such a sum is not possible");
+        printf("\nsuch a sum is not possible");
     }
     return 0;
 }
