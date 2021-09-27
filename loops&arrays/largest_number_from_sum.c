e0ffbf84c6bea8a2ae55490515ae671fe2894654
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-28 01:59:18 +0530

    Update largest_number_from_sum.c

--- loops&arrays/largest_number_from_sum.c
+++ loops&arrays/largest_number_from_sum.c
@@ -3,17 +3,34 @@
 int main()
 {
     int digits,sum;
+    printf("enter the number of digits and their sum :- ");
     scanf("%d %d",&digits,&sum);
-    int nines=0;
-    nines=sum/9;
-    sum=sum%9;
-    for (int i = 0; i < nines; i++) {
-        printf("9");
+    if(sum<=digits*9){
+        printf("largest number whose digits sum up to %d =",sum);
+        int nines=0;
+        nines=sum/9;
+        if(nines!=0){
+            sum=sum%9;
+            for (int i = 0; i < nines; i++) {
+                printf("9");
+            }
+            while(digits-nines!=0){
+                printf("%d",sum);
+                sum=sum/9;
+                digits--;
+            }
+        }
+        else{
+            printf("%d",sum);
+            digits--;
+            while (digits!=0){
+                printf("0");
+                digits--;
+            }
+        }
     }
-    while(digits-nines!=0){
-        printf("%d",sum);
-        sum=sum/9;
-        digits--;
+    else{
+        printf("\nsuch a sum is not possible");
     }
     return 0;
 }
