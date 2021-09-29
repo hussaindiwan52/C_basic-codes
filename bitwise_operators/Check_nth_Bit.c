03cee556fc22bd9b132c72f6eace557df5cea7f1
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-29 19:56:09 +0530

    Update Check_nth_Bit.c

--- bitwise_operators/Check_nth_Bit.c
+++ bitwise_operators/Check_nth_Bit.c
@@ -4,10 +4,7 @@ int main()
     int num,n;
     printf("enter number and nth place from least significant bit :-");
     scanf("%d %d",&num,&n);
-    while(num!=0 && n!=1){
-        num=num>>1;
-        n--;
-    }
+    num=num>>n-1;
     if(num&1){
         printf("nth bit was 1");
     }
