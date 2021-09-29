e36dfaf037a77f197d0e84ac74473606bf7fc5ec
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-29 19:41:25 +0530

    Update roman_numeral.c

--- loops&arrays/roman_numeral.c
+++ loops&arrays/roman_numeral.c
@@ -4,7 +4,7 @@ int main(){
     printf("enter a number:- ");
     scanf("%d",&num);
     char roman[][3]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-    int roman_values[]={1000,900,500,400,100,90,50,40,10,90,5,4,1};
+    int roman_values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
     int place_values[]={0,0,0,0,0,0,0,0,0,0,0,0,0};
     for (int i = 0; i < 13; i++)
     {
