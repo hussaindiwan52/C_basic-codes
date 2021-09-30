4baef538db10d40b7f0678c71b2eb1f08f9de0b4
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-09-30 13:46:39 +0530

    Create butterfly_pattern.c

---
+++ loops&arrays/pattern/butterfly_pattern.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{   int row=7;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j<=i; j++) {
+            printf("* ");
+        }
+        for (int j = row-1; j>i; j--) {
+            printf("  ");
+        }
+        for (int j = row; j>i+1; j--) {
+            printf("  ");
+        }
+        for (int j = 0; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < row; i++) {
+        for (int j = row; j>i; j--) {
+            printf("* ");
+        }
+        for (int j = 0; j<i; j++) {
+            printf("  ");
+        }
+        for (int j = 1; j<=i; j++) {
+            printf("  ");
+        }
+        for (int j = row; j>i; j--) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
