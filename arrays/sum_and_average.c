d58d058ecbcafc6d8c3e572ac0988734342a0fee
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-02 14:21:05 +0530

    Create sum_and_average.c

---
+++ arrays/sum_and_average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter size of array:- ");
+    scanf("%d",&n);
+    int arr[n];
+    int sum=0;
+    for (int i = 0; i < n; i++) {
+        printf("\nenter %d element :-",i);
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    float avg=(sum/n);
+    printf("%d %d %f",sum,n,avg);
+    return 0;
+}
