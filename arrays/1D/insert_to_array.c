23cbaa188264d97c065478f18c9a69fce19682e8
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-12 12:28:45 +0530

    Update and rename arrays/insert_to_array.c to arrays/1D/insert_to_array.c

--- arrays/1D/insert_to_array.c
+++ arrays/1D/insert_to_array.c
@@ -5,19 +5,19 @@ int main()
     int n;
     printf("enter size of array\n");
     scanf("%d",&n);
-    int n_e,n_i;
+    int new_elem,new_index;
     int arr[100];
     for(int i=0;i<n;i++){
         printf("enter %d element in first array :- \n",i);
         scanf("%d",&arr[i]);
     }
     printf("\nenter value to be added and its location :-\n");
-    scanf("%d",&n_e);
-    scanf("%d",&n_i);
-    for (int i = n; i >n_i; i--) {
+    scanf("%d",&new_elem);
+    scanf("%d",&new_index);
+    for (int i = n; i >new_index; i--) {
         arr[i]=arr[i-1];
     }
-    arr[n_i]=n_e;
+    arr[n_i]=new_elem;
     
     printf("array after value is inserted :- ");
     for(int i=0;i<n+1;i++){
