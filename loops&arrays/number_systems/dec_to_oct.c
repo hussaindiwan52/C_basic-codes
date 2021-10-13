e07411f5f9fc2479a2f33a9a16e99680991e290f
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-14 00:54:45 +0530

    Create dec_to_oct.c

---
+++ loops&arrays/number_systems/dec_to_oct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int arr[12];
+    scanf("%d",&num);
+    for (int i = 0; i < 12; i++) {
+        arr[i]=0;
+    }
+    int i=0;
+    while(num!=0){
+        arr[i]=num%8;
+        num/=8;
+        i++;
+    }
+    for (int i = 11; i >=0; i--) {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
