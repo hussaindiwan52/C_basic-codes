2f1ce164a863b6a896c574bd4a6e2e38ea04c5c4
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-14 17:17:37 +0530

    Create oct_to_bin.c

---
+++ loops&arrays/number_systems/oct_to_bin.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int oct,num_dec=0,bin=0,placeval=1;
+    scanf("%d",&oct);
+    while(oct!=0){
+        num_dec+=((oct%10)*placeval);
+        placeval*=8;
+        oct/=10;
+    }
+    int arr[32];
+    printf("decimal = %d\n",num_dec);
+    for (int i = 0; i < 32; i++) {
+        arr[i]=0;
+    }
+    int i=0;
+    while(num_dec!=0){
+        arr[i]=num_dec%2;
+        num_dec/=2;
+        i++;
+    }
+    for (int i = 31; i >=0; i--) {
+        bin*=10;
+        bin+=arr[i];
+    }
+    printf("binary = %d",bin);
+    return 0;
+}
