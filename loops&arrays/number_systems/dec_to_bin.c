470bb2d0ddd5eb6d9a0d0d18b46f3b6514700a72
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-15 17:35:12 +0530

    Update dec_to_bin.c

--- loops&arrays/number_systems/dec_to_bin.c
+++ loops&arrays/number_systems/dec_to_bin.c
@@ -4,6 +4,7 @@ int main()
 {
     int num,bin=0;
     int arr[32];
+    printf("decimal = ");
     scanf("%d",&num);
     for (int i = 0; i < 32; i++) {
         arr[i]=0;
@@ -18,6 +19,6 @@ int main()
         bin*=10;
         bin+=arr[i];
     }
-    printf("%d",bin);
+    printf("binary = %d",bin);
     return 0;
 }
