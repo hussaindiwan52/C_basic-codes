583cea35edf3d681e62f027b2deb2a2b71344ed6
Hussain Diwan <76115293+hussaindiwan52@users.noreply.github.com>
2021-10-19 00:41:14 +0530

    Update late_fees.c

--- if_else/late_fees.c
+++ if_else/late_fees.c
@@ -5,22 +5,22 @@ int main()
     int time_late,late_fee=0;
     printf("How late was the student :- ");
     scanf("%d",&time_late);
-    if(time_late>0){
-        late_fee=(100*((time_late<4)?time_late:3));
+    if(time_late>0 && time_late<=3){
+        late_fee=(100*(time_late));
     }
     
-    if(time_late>3){
-        late_fee=300 + (150*((time_late<8)?time_late-3:7));
+    else if(time_late>3 && time_late<=7){
+        late_fee=300 + (150*(time_late-3));
     }
     
-    if(time_late>7){
-        late_fee=900 + (250*((time_late<11)?time_late-7:10));
+    else if(time_late>7 && time_late<=10){
+        late_fee=900 + (250*(time_late-7));
     }
     
-    if(time_late>10){
+    else if(time_late>10){
         late_fee=1650 + (500*(time_late-10));
     }
     
-    printf("%d",late_fee);
+    printf("late fees = %d",late_fee);
     
 }
